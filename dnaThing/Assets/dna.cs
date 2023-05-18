0c08e6bdd123da4ce7ec0019ba60e1a165c42454
zanzilla22 <zanezilla22@gmail.com>
2023-05-17 22:55:56 -0400

    string thingy working yayyyyy
    yayy more like yayyyyy

--- dnaThing/Assets/dna.cs
+++ dnaThing/Assets/dna.cs
@@ -1,38 +1,65 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class DNAPair
+{
+    public char character;
+    public GameObject gameObject;
+}
+
 public class dna : MonoBehaviour
 {
-    public GameObject pair;
-    public int noPairs = 4;
-    public float rotation = 10;
+    public string codeIdk = "CCCCCCCCAAAAAAAAAAAAAAAAAAAAAAAAAAAAUUUUUUUUUUUUUUUUUUUGGGGGGUUUUUUUUUUUUCCCCCC";
+    public DNAPair[] dnaPairs;
+    public GameObject pairPrefab;
+    public float rotationOffset = 13f;
+    public float rotationSpeed = 10f;
     public float offset = 1f;
-    public GameObject[] pairs;
-    public int rotateSpeed = 30;
-    public GameObject[] basePairs;
 
-    void Start()
+    private GameObject[] pairs;
+
+    private void Start()
     {
-        pairs = new GameObject[noPairs]; // Initialize the array with the appropriate size
+        pairs = new GameObject[codeIdk.Length / 2]; // Initialize the array with the appropriate size
 
-        for (int i = 0; i < noPairs; i++)
+        for (int i = 0; i < codeIdk.Length - 1; i += 2)
         {
-            pairs[i] = Instantiate(pair, new Vector3(0, i * offset, 0), Quaternion.Euler(0, rotation * i, 0));
+            GameObject obj1 = GetGameObjectForCharacter(codeIdk[i]);
+            GameObject obj2 = GetGameObjectForCharacter(codeIdk[i + 1]);
 
-            GameObject randomObject = basePairs[Random.Range(0, basePairs.Length)];
-            pairs[i].GetComponent<pair>().bp1 = randomObject;
-
-            randomObject = basePairs[Random.Range(0, basePairs.Length)];
-            pairs[i].GetComponent<pair>().bp2 = randomObject;
-
-            pairs[i].GetComponent<pair>().instOb();
-
-            pairs[i].transform.parent = this.GetComponent<Transform>();
+            if (obj1 != null && obj2 != null)
+            {
+                int pairIndex = i / 2;
+                pairs[pairIndex] = Instantiate(pairPrefab, new Vector3(0, pairIndex * offset, 0), Quaternion.Euler(0, rotationOffset * pairIndex, 0));
+                pairs[pairIndex].GetComponent<pair>().bp1 = obj1;
+                pairs[pairIndex].GetComponent<pair>().bp2 = obj2;
+                pairs[pairIndex].GetComponent<pair>().instOb();
+                pairs[pairIndex].transform.parent = transform;
+            }
+            else
+            {
+                Debug.Log(codeIdk[i]);
+                Debug.Log(codeIdk[i + 1]);
+            }
         }
+
     }
-    void Update()
+
+    private void Update()
     {
-        this.GetComponent<Transform>().Rotate(0, rotateSpeed*Time.deltaTime, 0);
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+
+    private GameObject GetGameObjectForCharacter(char character)
+    {
+        foreach (var pair in dnaPairs)
+        {
+            if (pair.character == character)
+                return pair.gameObject;
+        }
+
+        Debug.LogError("No corresponding GameObject found for character: " + character);
+        return null;
     }
 }
